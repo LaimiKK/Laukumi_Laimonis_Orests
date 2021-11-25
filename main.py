a2c51d56981ec7818f7a833597786773f2b70509
Laimonis K <94110519+LaimiKK@users.noreply.github.com>
2021-11-25 09:55:40 +0000

    cc

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Lai aprēķināu laukumu nepieciešamai tev figūrai lūdzu nospied burtu
 print("T - ja figūra kaurai vēlies aprēķināt laukumu ir trijstūris")
 print("Tr - ja figūra kaurai vēlies aprēķināt laukumu ir trapece")
 print("P - ja figūra kaurai vēlies aprēķināt laukumu ir paralelograms")
-print("Ta - ja figūra kaurai velies aprēķināt laukumu ir taisnstūris")
-print("K - ja figūra kaurai velies aprēķināt laukumu ir kvadrāts")
+print("Ta - ja figūra kaurai vēlies aprēķināt laukumu ir taisnstūris")
+print("K - ja figūra kaurai vēlies aprēķināt laukumu ir kvadrāts")
 print("R - ja figūra kaurai vēlies aprēķināt laukumu ir riņķis")
 
