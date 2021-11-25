24cab18d7ad70c770fd735086e5f2e1bb209f871
Laimonis K <94110519+LaimiKK@users.noreply.github.com>
2021-11-25 11:31:14 +0000

    cc

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+def taisnsturis():
+  a= int(input("ievadi taisnstūra platumu m:"))
+  b= int(input("ievadi taisnstūra garumu m:"))
+  if a==b:
+    S=a*b
+    print("tas ir kvadrāts un tā laukums ir",S,"metri")
+  else:
+    S= a*b
+    print("Taisnstūra laukums ir", S,"metri")
+def kvadrats():
+  a= int(input("ievadi kvardāta malas garumu metros:"))
+  S=a*a
+  print("kvadrāta lukums ir",S,"metri")
+def rinkis():
+  r=int(input("ievadi riņķa rādiusu metros:"))
+  R2=r*r
+  S=3.14*R2
+  print("Riņķa laukums ir",S,"metri")
+
+
+
+
 print("Lai aprēķināu laukumu nepieciešamai tev figūrai lūdzu nospied burtu kurš tai atbilst")
 print("T - ja figūra kaurai vēlies aprēķināt laukumu ir trijstūris")
 print("Tr - ja figūra kaurai vēlies aprēķināt laukumu ir trapece")
@@ -6,3 +28,16 @@ print("Ta - ja figūra kaurai vēlies aprēķināt laukumu ir taisnstūris")
 print("K - ja figūra kaurai vēlies aprēķināt laukumu ir kvadrāts")
 print("R - ja figūra kaurai vēlies aprēķināt laukumu ir riņķis")
 
+x= input()
+if x == "Ta":
+  taisnsturis()
+
+
+  
+elif x== "K":
+  kvadrats()
+elif x=="R":
+  rinkis()
+else:
+  print("Ievadītā vērtība neatbilst prasītajām un uzdevumu izpildīt nav iespējams")
+   
